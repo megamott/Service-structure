43fcc9a97850b1ed87b070842e741462b8f9c461
Konoplyov Matvey <m.konoplyov@corp.mail.ru>
2023-11-07 22:37:15 +0300

    create users endpoint

--- app/api/common/views.py
+++ app/api/common/views.py
@@ -1,8 +1,17 @@
 from aiohttp import web
 
+from app.repositories.common import UserRepository
+from app.models.common import User
+
 router = web.RouteTableDef()
 
 
 @router.get("/ping/")
 async def ping(request: web.Request):
     return web.Response(text="OK")
+
+
+@router.get("/users/")
+async def users(request: web.Request):
+    users: list[User] = await UserRepository().all()
+    return web.Response(text=', '.join([user.name for user in users]))
