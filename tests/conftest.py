8defc6954543a95ee04eed0ab113bf36875e7897
Konoplyov Matvey <m.konoplyov@corp.mail.ru>
2023-11-10 08:59:42 +0300

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import json
+import asyncio
+from typing import Any
+
+import pytest
+from unittest import mock
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.configuration.config import Settings
+from app import create_app
+from app.configuration.models.base import Base
+from app.configuration.models.db_helper import DatabaseHelper
+
+
+settings = Settings(**{
+    "database_url": "postgresql+asyncpg://postgres:postgres@localhost:6432/postgres",
+    "database_echo": True,
+})
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+def app_settings_mock():
+    with mock.patch("app.configuration.config._read_settings", return_value=settings) as app_settings_mock:
+        yield app_settings_mock
+
+
+@pytest.fixture
+async def client(aiohttp_client: Any, app_settings_mock: mock.MagicMock):
+    app = await create_app()
+    return await aiohttp_client(app)
+
+
+@pytest.fixture(scope="session")
+async def db(app_settings_mock: mock.MagicMock):
+    db_helper = DatabaseHelper()
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    return db_helper
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def truncate_tables():
+    """ Чистит таблицы перед каждым тестом """
+    db_helper = DatabaseHelper()
+    session: AsyncSession
+    async with db_helper.session() as session:
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(table.delete())
+            await session.commit()
+    yield
---
+++ tests/users/test_views.py
@@ -0,0 +1,199 @@
+from aiohttp.test_utils import TestClient
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.users import User
+from configuration.models.db_helper import DatabaseHelper
+
+
+async def test_create_user_success(client: TestClient, db: DatabaseHelper) -> None:
+    data = {"name": "NAME"}
+    resp = await client.post("/api/v1/users/", json=data)
+    assert resp.status == 201
+
+    response_data = await resp.json()
+    assert response_data == {**data, "id": 1}
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+
+    assert len(users) == 1
+
+
+async def test_create_user_fail(client: TestClient, db: DatabaseHelper) -> None:
+    data = {"name": 15}
+    resp = await client.post("/api/v1/users/", json=data)
+    assert resp.status == 400
+
+    response_data = await resp.text()
+    assert response_data == "User data is not correct"
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+
+    assert len(users) == 0
+
+
+async def test_get_users_success(client: TestClient, db: DatabaseHelper) -> None:
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.get("/api/v1/users/")
+    assert resp.status == 200
+
+    response_data = await resp.json()
+    assert response_data == [{"name": user.name, "id": user.id}]
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+
+    assert len(users) == 1
+
+
+async def test_get_user_success(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.get(f"/api/v1/users/{user.id}/")
+    assert resp.status == 200
+
+    response_data = await resp.json()
+    assert response_data == {"name": user.name, "id": user.id}
+
+
+async def test_get_user_fail(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.get(f"/api/v1/users/hello/")
+    assert resp.status == 400
+
+    response_data = await resp.text()
+    assert response_data == "User id is not correct"
+
+    not_existed_user_id = 1488
+    resp = await client.get(f"/api/v1/users/{not_existed_user_id}/")
+    assert resp.status == 404
+
+    response_data = await resp.text()
+    assert response_data == f"User {not_existed_user_id} not found"
+
+
+async def test_update_user_success(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    new_name = "name"
+    resp = await client.patch(f"/api/v1/users/{user.id}/", json={"name": new_name})
+    assert resp.status == 200
+
+    response_data = await resp.json()
+    assert response_data == {"name": new_name, "id": user.id}
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+        assert len(users) == 1
+
+        user = await session.get(User, int(user.id))
+        assert user.name == new_name
+
+
+async def test_update_user_fail(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.patch(f"/api/v1/users/{user.id}/", json={"name": 100})
+    assert resp.status == 400
+
+    response_data = await resp.text()
+    assert response_data == "User data is not correct"
+
+    resp = await client.patch("/api/v1/users/hello/", json={"name": "new_name"})
+    assert resp.status == 400
+
+    response_data = await resp.text()
+    assert response_data == "User data is not correct"
+
+    not_existed_user_id = 1488
+    resp = await client.patch(f"/api/v1/users/{not_existed_user_id}/", json={"name": "new_name"})
+    assert resp.status == 404
+
+    response_data = await resp.text()
+    assert response_data == f"User {not_existed_user_id} not found"
+
+
+async def test_delete_user_success(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.delete(f"/api/v1/users/{user.id}/")
+    assert resp.status == 200
+
+    response_data = await resp.text()
+    assert response_data == "User deleted successfully"
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+        assert len(users) == 0
+
+
+async def test_delete_user_fail(client: TestClient, db: DatabaseHelper) -> None:
+    session: AsyncSession
+    async with db.session() as session:
+        user = User(name="NAME")
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+
+    resp = await client.delete(f"/api/v1/users/hello/")
+    assert resp.status == 400
+
+    response_data = await resp.text()
+    assert response_data == "User id is not correct"
+
+    not_existed_user_id = 1488
+    resp = await client.delete(f"/api/v1/users/{not_existed_user_id}/")
+    assert resp.status == 404
+
+    response_data = await resp.text()
+    assert response_data == f"User {not_existed_user_id} not found"
+
+    session: AsyncSession
+    async with db.session() as session:
+        result: Result = await session.execute(select(User).order_by(User.id))
+        users = result.scalars().all()
+        assert len(users) == 1
