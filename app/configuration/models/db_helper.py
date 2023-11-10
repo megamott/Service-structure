5e6db1a798d9461a0ab036b344ed216f96a7b5c4
Konoplyov Matvey <m.konoplyov@corp.mail.ru>
2023-11-10 08:58:21 +0300

    .remove() session after using

--- app/configuration/models/db_helper.py
+++ app/configuration/models/db_helper.py
@@ -1,4 +1,5 @@
 from asyncio import current_task
+from contextlib import asynccontextmanager
 
 from sqlalchemy.ext.asyncio import (AsyncSession, async_scoped_session,
                                     async_sessionmaker, create_async_engine)
@@ -7,26 +8,36 @@ from app.configuration.config import get_settings
 
 
 class DatabaseHelper:
-    def __init__(self, database_url: str, echo: bool) -> None:
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(DatabaseHelper, cls).__new__(cls)
+            cls.instance._initialized = False
+        return cls.instance
+
+    def __init__(self) -> None:
+        if self._initialized:
+            return
+        settings = get_settings()
+
         self.engine = create_async_engine(
-            url=database_url,
-            echo=echo,
+            url=settings.database_url,
+            echo=settings.database_echo,
         )
 
-        self.session_factory = async_sessionmaker(
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
 
-    def get_session(self) -> AsyncSession:
-        session = async_scoped_session(
+        self._initialized = True
+
+    @asynccontextmanager
+    async def session(self) -> AsyncSession:
+        scoped_factory = async_scoped_session(
             session_factory=self.session_factory,
             scopefunc=current_task,
         )
-        return session
-
-
-settings = get_settings()
-db_helper = DatabaseHelper(
-    database_url=settings.database_url,
-    echo=settings.database_echo,
-)
+        try:
+            async with scoped_factory() as s:
+                yield s
+        finally:
+            await scoped_factory.remove()
